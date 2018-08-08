75b4baaa0d14ff6ce12260cd0cf03cba000eea74
zyf1234567 <37658721+zyf1234567@users.noreply.github.com>
2018-08-08 20:23:22 +0800

    Zyf (#9)
    * test.py
    
    * 11.py

---
+++ 重复个数.py
@@ -0,0 +1,10 @@
+from collections import Collections
+def main():
+    words = [
+        'look','into','my','eyes','good','methods',
+        'look','eyes','look'
+    ]
+    collect = Collections()
+    collect.counter(words)
+if __name__ == '__main__':
+    main()
